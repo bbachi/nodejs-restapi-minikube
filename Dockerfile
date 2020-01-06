44bc28b3ce1566e68272a3b91cc4c080360c466d
Bhargav Bachina <bhargavbachina@yahoo.com>
2020-01-06 15:17:56 -0600

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,10 @@
 # stage1 as builder
 FROM node:10-alpine
 
-# copy the package.json to install dependencies
-COPY package.json package-lock.json ./
-
-# Install the dependencies and make the folder
-RUN npm install && mkdir /api && mv ./node_modules ./api
-
 WORKDIR /api
 
 COPY . .
 
 EXPOSE 3000
 
-ENTRYPOINT ["npm", "start"]
+ENTRYPOINT ["node", "index.js"]
--- index.js
+++ index.js
@@ -1,18 +1,36 @@
-const express = require('express');
-const app = express();
+const http = require('http');
+const qs = require("querystring");
+const url = require('url');
 
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-    res.send("<h1>Hello World</h1>")
+const server = http.createServer((req, res) => {
+
+    if (req.method !== 'GET') handleError(405, res);
+
+    const {pathname, query} = url.parse(req.url);
+
+    if (pathname === '/name') {
+        const {name} = qs.parse(query);
+        if (name) {
+            res.end(`<h1>You Entered: ${name.toUpperCase()}</h1>`)
+        } else {
+            res.end("Bad Request");
+        }
+        
+    }
+
+    if (pathname === '/') {
+        res.end(`<h1>Hello World</h1>`)
+    }
 });
 
-app.get('/name/:myname', (req, res) => {
-    const myName = req.params.myname
-    res.send(`<h1>You entered:  ${myName}</h1>`)
-})
+function handleError(code, res) {
+    res.statusCode = code;
+    res.end(`{"error": "${http.STATUS_CODES[code]}"}`);
+}
 
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
-})
